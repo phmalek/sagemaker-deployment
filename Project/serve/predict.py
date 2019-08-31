a70111f7dcbea628e1898336c8e31a36888cfc08
Ali Malek <ph.malek@gmail.com>
2019-08-31 17:44:40 +0000

    predict.py is done, still not tested

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,9 +69,8 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    review = review_to_words(input_data)
+    data_X, data_ln = convert_and_pad(model.word_dict,review)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -84,9 +83,11 @@ def predict_fn(input_data, model):
     # Make sure to put the model into evaluation mode
     model.eval()
 
+    with torch.no_grad(): #speeding up and reducing memory usage
+        output = model(data)
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    result = np.round(output.numpy())
 
     return result
